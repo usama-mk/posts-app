8bf03bb6117d926dec003c9d995a4caddb6ac837
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-07-13 21:24:34 +0500

    updated better

--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
 import './Comment.scss'
 
 
 export default function Comment(props) {
     const {comment} = props;
-
+    const user= useSelector(state => state.user.user)
+  const additionalData= useSelector(state => state.user.additionalData)
     useEffect(()=>{
         // console.log(comment.commentMessage)
     },[])
     return (
         <div className="comment">
              
-    <span style={{color:"white", fontSize:"bolder", backgroundColor:"grey"}} className="commentEmail">{comment.name}</span>
+    <span style={{color:"white", fontSize:"bolder", backgroundColor:"grey"}} className="commentEmail">{user.uid==comment.uid?additionalData.name:comment.name?comment.name:comment.name}</span>
     <h3 style={{color:"black", fontSize:"bolder", backgroundColor:"grey" }}className="commentMessage">{comment.commentMessage}</h3>
      
              
